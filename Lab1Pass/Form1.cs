c777f529b346e131170bbb290bcc84af378e7894
was80 <was80@IGOR777>
2021-10-17 17:56:38 +0300

    Добавьте файлы проекта.

---
+++ Lab1Pass/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Lab1Pass
+{
+    public partial class Authorization : Form
+    {
+        public Authorization()
+        {
+            InitializeComponent();
+        }
+
+        private void Input_Click(object sender, EventArgs e)
+        {
+
+
+            if (Login.Text == "Igor" && Password.Text == "0000")
+            {
+                Login.BackColor = Color.Green;
+                Password.BackColor = Color.Green;
+                MessageBox.Show("Вхід виконано");
+            }
+            else
+            {
+                Login.BackColor = Color.Red;
+                Password.BackColor = Color.Red;
+                MessageBox.Show("Невірний логін або пароль!");
+            }
+        }
+    }
+}
